53834c9a36bcb6ac6dfd6bebdf0d7119311bd0e8
wenwei63029869 <wwen1030@gmail.com>
2016-02-27 16:56:40 -0800

    upload angular template

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name eventMeetApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the eventMeetApp
+ */
+angular.module('eventMeetApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name eventMeetApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the eventMeetApp
+ */
+angular.module('eventMeetApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
