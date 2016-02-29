c13f3b15758cddfa1ea1b24dd770adefcfb812ea
wenwei63029869 <wwen1030@gmail.com>
2016-02-28 18:54:26 -0800

    add rooms page and install ui-bootstrap

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,28 +8,45 @@
  *
  * Main module of the application.
  */
+
 angular
-  .module('eventMeetApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+ .module('eventMeetApp', [
+  'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ui.router',
+  'ngSanitize',
+  'ngTouch'
+])
+.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
+  .state('homepage', {
+    url: '/',
+    templateUrl: 'views/_homepage.html',
+    controller: 'HomepageCtrl'
+  })
+  .state('roompage', {
+    url: '/rooms',
+    templateUrl: 'views/_rooms.html',
+    controller: 'RoomsCtrl',
+    resolve: {
+
+    }
   });
+})
+
+/**
+ * rooms table js
+ */
+$(document).ready(function () {
+  $(".room-button").click(function(){
+    console.log("hit room button");
+    $("#header").css("background-color", "#383b43");
+  })
+
+  $(".home-button").click(function(){
+    console.log("hit home button");
+    $("#header").css("background-color", "transparent");
+  })
+});
